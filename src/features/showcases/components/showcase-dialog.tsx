411c01af6f006328e71bcc907f018d668a196ea6
kazuvin <166889742+kazuvin@users.noreply.github.com>
2026-01-18 18:16:55 +0900

    fix: dialog

--- src/features/showcases/components/showcase-dialog.tsx
+++ src/features/showcases/components/showcase-dialog.tsx
@@ -16,12 +16,12 @@ export type ShowcaseDialogProps = {
 export function ShowcaseDialog({ item, open, onOpenChange }: ShowcaseDialogProps) {
   return (
     <Dialog open={open} onOpenChange={onOpenChange}>
-      <DialogContent size="lg" className="flex max-h-[80vh] flex-col">
-        <DialogHeader>
+      <DialogContent size="lg" className="!p-0 flex max-h-[80vh] flex-col">
+        <DialogHeader className="border-foreground/10 border-b p-6">
           <DialogTitle>{item?.name}</DialogTitle>
           <DialogDescription>{item?.description}</DialogDescription>
         </DialogHeader>
-        <div className="mt-4 flex-1 overflow-y-auto">{item?.fullDemo}</div>
+        <div className="flex-1 overflow-y-auto p-6">{item?.fullDemo}</div>
       </DialogContent>
     </Dialog>
   );
